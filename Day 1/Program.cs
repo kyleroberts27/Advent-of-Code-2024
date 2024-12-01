3bdd84ff6cba1709a3d3af09b9dbed444acd0b77
kyleroberts27 <6robek81@solent.ac.uk>
2024-12-01 16:05:23 +0000

    Part One of Day One

--- Day 1/Program.cs
+++ Day 1/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Data.Common;
+using System.Runtime.CompilerServices;
 
 namespace Day_1
 {
@@ -11,15 +13,48 @@ namespace Day_1
     {
         static void Main(string[] args)
         {
-            using (StreamReader sr = new StreamReader("Input.txt"))
+
+            var file = "Input.txt";
+            using (StreamReader sr = new StreamReader(file))
             {
                 string line;
 
+                var locationIDLeftList = new List<int>();
+
+                var locationIDRightList = new List<int>();
+
                 while ((line = sr.ReadLine()) != null)
                 {
-                    Console.WriteLine(line);
+                    var parts = line.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
+
+                    if (int.TryParse(parts.First(), out var number))
+                    {
+                        locationIDLeftList.Add(number);
+                    }
+
+                    if (int.TryParse(parts.Last(), out number))
+                    {
+                        locationIDRightList.Add(number);
+                    }
+
                 }
+
+                locationIDLeftList.Sort();
+
+                locationIDRightList.Sort();
+
+                var total = 0;
+
+                for (int i = 0; i < locationIDLeftList.Count; i++)
+                {
+                    total += Math.Abs(locationIDLeftList[i] - locationIDRightList[i]);
+                }
+
+                Console.WriteLine(total);
+
+
             }
         }
+
     }
 }
